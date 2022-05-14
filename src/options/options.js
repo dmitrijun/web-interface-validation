cc373227b193a63c3366a4d35d2de18cee6417c0
Dmitry Malkov <domalkov@edu.hse.ru>
2022-05-14 20:42:28 +0300

    Added more options to vary

--- src/options/options.js
+++ src/options/options.js
@@ -1,14 +1,32 @@
 function loadOptions() {
   chrome.storage.local.get(["settings"], (result) => {
     document.getElementById("endpoint").value = result.settings.endpoint;
+    document.getElementById("fullXPath").checked = result.settings.fullXPath;
+    document.getElementById("saveActions").checked =
+      result.settings.saveActions;
+    document.getElementById("saveCoordinates").checked =
+      result.settings.saveCoordinates;
   });
 }
 
 function saveOptions() {
-  const value = document.getElementById("endpoint").value;
-  chrome.storage.local.set({ endpoint: value }, () => {
-    console.log("Updated settings successfully");
-  });
+  const endpoint = document.getElementById("endpoint").value;
+  const fullXPath = document.getElementById("fullXPath").checked;
+  const saveActions = document.getElementById("saveActions").checked;
+  const saveCoordinates = document.getElementById("saveCoordinates").checked;
+  chrome.storage.local.set(
+    {
+      settings: {
+        endpoint: endpoint,
+        fullXPath: fullXPath,
+        saveActions: saveActions,
+        saveCoordinates: saveCoordinates,
+      },
+    },
+    () => {
+      console.log("Updated settings successfully");
+    }
+  );
 }
 
 window.onload = function () {
